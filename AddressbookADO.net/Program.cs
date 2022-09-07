d8f7080471f4788d93f189e873f0e99a37cfd8f9
aayush-aryan <log2aayusharyan@gmail.com>
2022-09-07 23:10:11 +0530

    added. welcome measage for AddressBook using Ado.net

---
+++ AddressbookADO.net/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AddressbookADO.net
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to AddressBook ADO.net");
+        }
+    }
+}
